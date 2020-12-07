e9e1325b9aec4da9cf060ef7669f351909804d58
Fallenefc <arylmoraesn@hotmail.com>
2020-12-07 09:34:42 -0600

    refactored how we return data

--- src/utils/visitDateSet.ts
+++ src/utils/visitDateSet.ts
@@ -1,9 +1,8 @@
 /* eslint-disable max-len */
 const generateDatesSet = (allVisits: any) => {
   const dates = allVisits.map((visit: any) => visit.date);
-  console.log(typeof dates[0]);
-  const reducedArray = dates.reduce((unique: any, item: any) => unique.includes((item) ? unique : [...unique, item]));
-  return reducedArray;
+  const datesSet = new Set(dates);
+  return datesSet;
 };
 
 export default generateDatesSet;
